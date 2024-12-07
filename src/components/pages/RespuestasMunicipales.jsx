b833db1e32de65176b10d129f05947c05370ac3f
edv <eduardo.bautista@inacapmail.cl>
2024-12-07 20:33:40 -0300

    fx sidebar en respuestas municipales

--- src/components/pages/RespuestasMunicipales.jsx
+++ src/components/pages/RespuestasMunicipales.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import TopBar from '../TopBar'
 const RespuestasMunicipales = ({ isOpened, setIsOpened }) => {
+  const handleOpenSidebar = () => {
+    setIsOpened(!isOpened)
+  }
   return (
     <>
-      <TopBar title="Respuestas Municipales" isOpened={isOpened} setIsOpened={setIsOpened} />
+      <TopBar title="Respuestas Municipales" handleOpenSidebar={handleOpenSidebar} />
       
       
     </>
