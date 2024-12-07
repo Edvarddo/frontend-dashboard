558a23fefb93bc90ab45a53714b62a5ccc639aea
edv <eduardo.bautista@inacapmail.cl>
2024-12-07 20:30:30 -0300

    Añadir archivo respuestas municipales

---
+++ src/components/pages/RespuestasMunicipales.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TopBar from '../TopBar'
+const RespuestasMunicipales = ({ isOpened, setIsOpened }) => {
+  return (
+    <>
+      <TopBar title="Respuestas Municipales" isOpened={isOpened} setIsOpened={setIsOpened} />
+      
+      
+    </>
+  )
+}
+
+export default RespuestasMunicipales
